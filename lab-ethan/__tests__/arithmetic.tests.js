6cf08316d42da2ef57a7ee7ba9bdad13b1bcf75c
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-13 20:33:21 -0500

    wrote test for if integers were given

--- lab-ethan/__tests__/arithmetic.tests.js
+++ lab-ethan/__tests__/arithmetic.tests.js
@@ -6,4 +6,11 @@ describe('add',() => {
     expect(arithmetic.add('[1,2,3]','hello')).toBe(null);
     expect(arithmetic.add({type:'banana'},true)).toBe(null);
   });
+
+  it('adds two integers together', ()=> {
+    expect(arithmetic.add(0,0)).toBe(0);
+    expect(arithmetic.add(400,1)).toBe(401);
+    expect(arithmetic.add(-7,7.5)).toBe(0.5);
+    expect(arithmetic.add(-8,-2)).toBe(-10);
+  });
 });
